89613ed7004b5f6131bd016503cc34f52bf1e3b6
SunnieP <65377315+SunnieP@users.noreply.github.com>
2026-08-19 21:00:38 -0700

    Fix recursive jms_crafting:openBench event causing script deadloop

--- client/main.lua
+++ client/main.lua
@@ -1,7 +1,3 @@
-RegisterCommand('craft', function()
-    TriggerEvent('jms_crafting:openBench', 'starter_electronics_bench')
-end, false)
-
-RegisterNetEvent('jms_crafting:openBench', function(benchId)
-    TriggerEvent('jms_crafting:openBench', benchId)
-end)
+RegisterCommand('craft', function()
+    TriggerEvent('jms_crafting:openBench', 'starter_electronics_bench')
+end, false)
