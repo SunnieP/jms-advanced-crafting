88d5b137f02f7fdede97c37f4b38140fc5f4d67c
SunnieP <65377315+SunnieP@users.noreply.github.com>
2026-08-19 22:33:42 -0700

    Add rubber and female_rose items for Female Rose recipe

--- shared/items.lua
+++ shared/items.lua
@@ -3,5 +3,7 @@ JMSCraftingItems = {
     battery = { label = 'Battery', weight = 10 },
     cheap_phone_charger = { label = 'Cheap Phone Charger', weight = 1 },
     screwdriver = { label = 'Screwdriver', weight = 10 },
-    meta_glasses = { label = 'Meta Glasses', weight = 20 }
+    meta_glasses = { label = 'Meta Glasses', weight = 20 },
+    rubber = { label = 'Rubber', weight = 2 },
+    female_rose = { label = 'Female Rose', weight = 12 }
 }
