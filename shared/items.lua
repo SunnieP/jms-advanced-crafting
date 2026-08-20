f2c69694fabeee1ee7dcba061968db7657970498
SunnieP <65377315+SunnieP@users.noreply.github.com>
2026-08-19 21:35:59 -0700

    Align brokenglasses item key with ak47_prospecting loot table

--- shared/items.lua
+++ shared/items.lua
@@ -1,7 +1,7 @@
-JMSCraftingItems = {
-    broken_glasses = { label = 'Broken Glasses', weight = 5 },
-    battery = { label = 'Battery', weight = 10 },
-    cheap_phone_charger = { label = 'Cheap Phone Charger', weight = 1 },
-    screwdriver = { label = 'Screwdriver', weight = 10 },
-    meta_glasses = { label = 'Meta Glasses', weight = 20 }
-}
+JMSCraftingItems = {
+    brokenglasses = { label = 'Broken Glasses', weight = 5 },
+    battery = { label = 'Battery', weight = 10 },
+    cheap_phone_charger = { label = 'Cheap Phone Charger', weight = 1 },
+    screwdriver = { label = 'Screwdriver', weight = 10 },
+    meta_glasses = { label = 'Meta Glasses', weight = 20 }
+}
